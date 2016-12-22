adb363e8bc162d7e705054ea7108fa96af51858f
Luke Childs <lukechilds123@gmail.com>
2016-12-22 18:53:40 +0700

    Extract expired from onionoo-node-client

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const addSeconds = require('date-fns/add_seconds')
+
+function expired (headers) {
+  const originDate = new Date(headers.date)
+
+  // Get max age ms
+  let maxAge = headers['cache-control'] && headers['cache-control'].match(/max-age=(\d+)/)
+  maxAge = parseInt(maxAge ? maxAge[1] : 0)
+
+  // Take current age into account
+  if (headers.age) {
+    maxAge -= headers.age
+  }
+
+  // Calculate expirey date
+  return addSeconds(new Date(originDate), maxAge)
+}
+
+module.exports = expired
