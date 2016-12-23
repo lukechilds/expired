3b6a6d777580a66ed326fdbebaaf0f23433fd054
Luke Childs <lukechilds123@gmail.com>
2016-12-23 11:23:00 +0700

    Test expired.in returns positive ms for valid cache

--- test/expired.in.js
+++ test/expired.in.js
@@ -1,6 +1,21 @@
 import test from 'ava';
+import tk from 'timekeeper';
 import expired from '../';
 
 test('expired.in is a function', t => {
 	t.is(typeof expired.in, 'function');
+});
+
+test('expired.in returns positive ms for valid cache', t => {
+	const date = new Date().toUTCString();
+	const maxAge = 300;
+	const headers = {
+		date: date,
+		age: 0,
+		'cache-control': `public, max-age=${maxAge}`
+	};
+
+	tk.freeze(date);
+	t.is(expired.in(headers), maxAge * 1000);
+	tk.reset();
 });
