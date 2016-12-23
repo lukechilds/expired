6f7ae73dca122a876c7f8880051d67fcb260118c
Luke Childs <lukechilds123@gmail.com>
2016-12-23 10:08:53 +0700

    Test expired returns false for valid cache

--- test/expired.js
+++ test/expired.js
@@ -4,3 +4,13 @@ import expired from '../';
 test('expired is a function', t => {
 	t.is(typeof expired, 'function');
 });
+
+test('expired returns false for valid cache', t => {
+	const headers = {
+		date: new Date().toUTCString(),
+		age: 0,
+		'cache-control': 'public, max-age=300'
+	};
+
+	t.false(expired(headers));
+});
