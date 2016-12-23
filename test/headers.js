1c773154ae1cad4e300bd52edaeadfe2f046cff8
Luke Childs <lukechilds123@gmail.com>
2016-12-23 12:10:22 +0700

    Test headers can be passed in as a string

--- test/headers.js
+++ test/headers.js
@@ -15,3 +15,15 @@ test('headers can be passed in as an object', t => {
 	t.true(isEqual(expired.on(headers), date));
 	tk.reset();
 });
+
+test('headers can be passed in as a string', t => {
+	const date = new Date().toUTCString();
+	const headers = `
+		Date: ${date}
+		Age: 0
+		Cache-Control public, max-age=0`;
+
+	tk.freeze(date);
+	t.true(isEqual(expired.on(headers), date));
+	tk.reset();
+});
