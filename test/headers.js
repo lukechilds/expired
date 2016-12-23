6a9051d1389e41cda1e4335413e312984f2a353e
Luke Childs <lukechilds123@gmail.com>
2016-12-23 12:06:40 +0700

    Test headers can be passed in as an object

---
+++ test/headers.js
@@ -0,0 +1,17 @@
+import test from 'ava';
+import tk from 'timekeeper';
+import isEqual from 'date-fns/is_equal';
+import expired from '../';
+
+test('headers can be passed in as an object', t => {
+	const date = new Date().toUTCString();
+	const headers = {
+		date: date,
+		age: 0,
+		'cache-control': `public, max-age=0`
+	};
+
+	tk.freeze(date);
+	t.true(isEqual(expired.on(headers), date));
+	tk.reset();
+});
